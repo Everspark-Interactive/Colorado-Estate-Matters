249d445c8b963246d7d8fe68a10d9776b0930357
SabrinaMatthews <sm@ggalmightydigital.com>
2024-04-24 01:38:16 -0400

    Updating FAQs templates to render properly.

--- wp-phillipsandblow-theme/functions.php
+++ wp-phillipsandblow-theme/functions.php
@@ -7,44 +7,43 @@ Summary:
     Create a name, a path, and set a dependency array for each individual script found, enque each one.
     Finally, register and enque the main dot js once all others have been configured.
  --------------------------------------------------------------------------------------- */
-add_action('wp_enqueue_scripts', function(){
+add_action('wp_enqueue_scripts', function () {
   //! Required Enqueues
   wp_enqueue_script('jquery'); // Do not deregister, jQ is updated and registered automatically by WP
 
   $dirJS = new DirectoryIterator(get_stylesheet_directory() . '/js');
 
   foreach ($dirJS as $file) {
-      if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
-          $fullName = basename($file);
-          $name = substr(basename($fullName), 0, strpos(basename($fullName), '.'));
+    if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
+      $fullName = basename($file);
+      $name = substr(basename($fullName), 0, strpos(basename($fullName), '.'));
 
-          // Set Dependencies for any script.
-          // Since jQuery is already set as a priority requirement no need to list it as a dependency here.
-          switch($name) {
+      // Set Dependencies for any script.
+      // Since jQuery is already set as a priority requirement no need to list it as a dependency here.
+      switch ($name) {
 
-              // case 'examplename':
-              //     $deps = array('exampleDependency');
-              //     break;
+          // case 'examplename':
+          //     $deps = array('exampleDependency');
+          //     break;
 
-              default:
-                  $deps = null;
-                  break;
-
-          }
-
-          wp_enqueue_script( $name, get_template_directory_uri() . '/js/' . $fullName, $deps, '', true );
+        default:
+          $deps = null;
+          break;
       }
+
+      wp_enqueue_script($name, get_template_directory_uri() . '/js/' . $fullName, $deps, '', true);
+    }
   }
 
   // Main is Custom Coded by the 1P21 Team.
   // ( Used with an IF Statement, for Legacy Compatability )
-  if(file_exists(get_template_directory() .'/main.js')):
-      wp_register_script('mainjs', get_template_directory_uri() .'/main.js', '', '', true);
-      wp_enqueue_script('mainjs');
+  if (file_exists(get_template_directory() . '/main.js')) :
+    wp_register_script('mainjs', get_template_directory_uri() . '/main.js', '', '', true);
+    wp_enqueue_script('mainjs');
   endif;
-},10);
+}, 10);
 
-add_action('init', function() {
+add_action('init', function () {
   register_nav_menus(array( // Using array to specify more menus if needed
     'header-menu' => __('Main Menu', ''),
   ));
@@ -52,16 +51,17 @@ add_action('init', function() {
 
 /*External Files
 --------------------------------------------------------------------------------------- */
-require_once trailingslashit( dirname(__FILE__) ) . 'includes/custom-functions.php';
+require_once trailingslashit(dirname(__FILE__)) . 'includes/custom-functions.php';
 
 /* Combine all css, minify and inject in the head
  --------------------------------------------------------------------------------------- */
 
-add_action( 'wp_head', 'merge_include_css' );
-function merge_include_css() {
-  $cssDirLastMod = filemtime(get_stylesheet_directory().'/css');
-  $styleLastMod = filemtime(get_stylesheet_directory().'/style.css');
-  $_cssLastMod = filemtime(get_stylesheet_directory().'/speed/_css.php');
+add_action('wp_head', 'merge_include_css');
+function merge_include_css()
+{
+  $cssDirLastMod = filemtime(get_stylesheet_directory() . '/css');
+  $styleLastMod = filemtime(get_stylesheet_directory() . '/style.css');
+  $_cssLastMod = filemtime(get_stylesheet_directory() . '/speed/_css.php');
 
   if ($cssDirLastMod > $_cssLastMod || $styleLastMod > $_cssLastMod) {
     $theme = wp_get_theme();
@@ -69,13 +69,13 @@ function merge_include_css() {
 
     $cssFiles = glob(get_stylesheet_directory() . '/css/*.css');
 
-    foreach($cssFiles as $file) {
+    foreach ($cssFiles as $file) {
       $mergedCSS .= file_get_contents($file);
     }
 
-    $styles = file_get_contents( get_stylesheet_directory().'/style.css' );
-    $styles = str_replace( "images", site_url()."/wp-content/themes/$theme->template/images", $styles );
-    $styles = str_replace( "fonts", site_url()."/wp-content/themes/$theme->template/fonts", $styles );
+    $styles = file_get_contents(get_stylesheet_directory() . '/style.css');
+    $styles = str_replace("images", site_url() . "/wp-content/themes/$theme->template/images", $styles);
+    $styles = str_replace("fonts", site_url() . "/wp-content/themes/$theme->template/fonts", $styles);
 
     $mergedCSS .= $styles;
 
@@ -83,14 +83,14 @@ function merge_include_css() {
     $mergedCSS = str_replace(': ', ':', $mergedCSS);
     $mergedCSS = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $mergedCSS);
 
-    file_put_contents( get_stylesheet_directory().'/speed/_css.php', $mergedCSS );
+    file_put_contents(get_stylesheet_directory() . '/speed/_css.php', $mergedCSS);
 
     echo '<style>';
-    include_once get_stylesheet_directory().'/speed/_css.php';
+    include_once get_stylesheet_directory() . '/speed/_css.php';
     echo '</style>';
   } else {
     echo '<style>';
-    include_once get_stylesheet_directory().'/speed/_css.php';
+    include_once get_stylesheet_directory() . '/speed/_css.php';
     echo '</style>';
   }
 }
@@ -102,7 +102,7 @@ function merge_include_css() {
 /**
  * Gravity Forms Tab Conflict Fix
  */
-add_filter( 'gform_tabindex', '__return_false' );
+add_filter('gform_tabindex', '__return_false');
 
 
 
@@ -110,7 +110,7 @@ add_filter( 'gform_tabindex', '__return_false' );
 /* Blog Sidebar Widget
  --------------------------------------------------------------------------------------- */
 
-if(function_exists('register_sidebars')){
+if (function_exists('register_sidebars')) {
   register_sidebar(array(
     'name'          => 'Blog Sidebar',
     'id'            => 'blog_sidebar',
@@ -130,9 +130,9 @@ if(function_exists('register_sidebars')){
     'before_title'  => '<h3 class="widgettitle">',
     'after_title'   => '<i></i></h3>'
   ));
-	
-	register_sidebar(array(
-    'name'          => 'Faqs Sidebar',
+
+  register_sidebar(array(
+    'name'          => 'FAQs Sidebar',
     'id'            => 'faqs_sidebar',
     'description'   => '',
     'before_widget' => '<div id="%1$s" class="widget %2$s">',
@@ -149,7 +149,7 @@ if(function_exists('register_sidebars')){
 /* Add Theme Support Page Thumbnails
  --------------------------------------------------------------------------------------- */
 
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
 
 
@@ -158,7 +158,8 @@ add_theme_support( 'post-thumbnails' );
 /* Modify the_excerpt() "read more"
  --------------------------------------------------------------------------------------- */
 
-function new_excerpt_more($more) {
+function new_excerpt_more($more)
+{
   global $post;
   //return '... <a href="'. get_permalink($post->ID) . '">' . 'read more' . '</a>';
 }
@@ -171,16 +172,15 @@ add_filter('excerpt_more', 'new_excerpt_more');
 /* Theme Options for ACF
  --------------------------------------------------------------------------------------- */
 
-if( function_exists('acf_add_options_page') ) {
+if (function_exists('acf_add_options_page')) {
 
   acf_add_options_page(array(
-    'page_title' 	=> 'Theme Options',
-    'menu_title'	=> 'Theme Options',
-    'menu_slug' 	=> 'theme-options',
-    'capability'	=> 'edit_posts',
-    'redirect'		=> false
+    'page_title'   => 'Theme Options',
+    'menu_title'  => 'Theme Options',
+    'menu_slug'   => 'theme-options',
+    'capability'  => 'edit_posts',
+    'redirect'    => false
   ));
-
 }
 
 
@@ -209,13 +209,15 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 
 // Allow SVG
 
-function cc_mime_types($mimes) {
+function cc_mime_types($mimes)
+{
   $mimes['svg'] = 'image/svg+xml';
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
 
-function fix_svg_thumb_display() {
+function fix_svg_thumb_display()
+{
   echo '
     <style>td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { 
       width: 100% !important; 
@@ -226,196 +228,148 @@ function fix_svg_thumb_display() {
 }
 add_action('admin_head', 'fix_svg_thumb_display');
 
-function render_shortcode_blog_category_filter(){
-    $terms = get_categories();
-    $current_cat = false;
-    if ( is_category() && is_archive() ) {
-        $current_cat = single_cat_title('', false);
-    }
-    ob_start();
-    ?>
-    <div id="categories-filter-container">
-        <select id="blog-categories-filter" class="blog-categories-dropdown">
-            <option value="<?php echo get_permalink(get_option('page_for_posts')); ?>">All Categories</option>
-            <?php
-            foreach ($terms as $term) { ?>
-                <option value="<?php echo get_category_link($term->term_id); ?>" <?php echo $term->name === $current_cat ? 'selected' : ''; ?>><?php echo $term->name; ?></option>
-            <?php }
-            ?>
-        </select>
-    </div>
-    <?php
-    wp_enqueue_script('blog-category-filter');
-    return ob_get_clean();
+function render_shortcode_blog_category_filter()
+{
+  $terms = get_categories();
+  $current_cat = false;
+  if (is_category() && is_archive()) {
+    $current_cat = single_cat_title('', false);
+  }
+  ob_start();
+?>
+  <div id="categories-filter-container">
+    <select id="blog-categories-filter" class="blog-categories-dropdown">
+      <option value="<?php echo get_permalink(get_option('page_for_posts')); ?>">All Categories</option>
+      <?php
+      foreach ($terms as $term) { ?>
+        <option value="<?php echo get_category_link($term->term_id); ?>" <?php echo $term->name === $current_cat ? 'selected' : ''; ?>><?php echo $term->name; ?></option>
+      <?php }
+      ?>
+    </select>
+  </div>
+<?php
+  wp_enqueue_script('blog-category-filter');
+  return ob_get_clean();
 }
-add_shortcode( 'blog_category_filter', 'render_shortcode_blog_category_filter' );
+add_shortcode('blog_category_filter', 'render_shortcode_blog_category_filter');
 
-function enqueue_the_scripts() {
-    wp_register_script( 'blog-category-filter', get_stylesheet_directory_uri() . '//blog-category-filter.js', array( 'jquery' ), '1.0', true );
+function enqueue_the_scripts()
+{
+  wp_register_script('blog-category-filter', get_stylesheet_directory_uri() . '//blog-category-filter.js', array('jquery'), '1.0', true);
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_the_scripts' );
+add_action('wp_enqueue_scripts', 'enqueue_the_scripts');
 
 
 
 
 
-function render_shortcode_faqs_category_filter() {
-    $terms = get_terms(array(
-        'taxonomy' => 'faqs_category',
-        'hide_empty' => true,
-    ));
-    $current_cat = false;
-    if ( is_tax('faqs_category') ) {
-        $current_cat = get_queried_object()->name;
-    }
-    ob_start();
-    ?>
-    <div id="categories-filter-container">
-        <select id="faqs-categories-filter" class="faqs-categories-dropdown" onchange="location = this.value;">
-            <option value="<?php echo get_post_type_archive_link('faqs'); ?>">All Categories</option>
-            <?php foreach ($terms as $term) { ?>
-                <option value="<?php echo esc_url(get_term_link($term)); ?>" <?php echo $term->name === $current_cat ? 'selected' : ''; ?>><?php echo $term->name; ?></option>
-            <?php } ?>
-        </select>
-    </div>
-    <?php
-    return ob_get_clean();
+function render_shortcode_faqs_category_filter()
+{
+  $terms = get_terms(array(
+    'taxonomy' => 'faqs_category',
+    'hide_empty' => false,
+  ));
+  $current_cat = false;
+  if (is_tax('faqs_category')) {
+    $current_cat = get_queried_object()->name;
+  }
+  ob_start();
+?>
+  <div id="categories-filter-container">
+    <select id="faqs-categories-filter" class="faqs-categories-dropdown" onchange="location = this.value;">
+      <option value="<?php echo get_post_type_archive_link('faqs'); ?>">All Categories</option>
+      <?php foreach ($terms as $term) { ?>
+        <option value="<?php echo esc_url(get_term_link($term)); ?>" <?php echo $term->name === $current_cat ? 'selected' : ''; ?>><?php echo $term->name; ?></option>
+      <?php } ?>
+    </select>
+  </div>
+<?php
+  return ob_get_clean();
 }
-
-add_shortcode( 'faqs_category_filter', 'render_shortcode_faqs_category_filter' );
-
+add_shortcode('faqs_category_filter', 'render_shortcode_faqs_category_filter');
 
 
+function faq_categories_shortcode($atts)
+{
+  // Shortcode attributes
+  $atts = shortcode_atts(array(
+    'orderby' => 'name', // Default ordering
+    'order' => 'ASC', // Default order
+    'hide_empty' => false, // Show even if categories have no posts
+    'taxonomy' => 'faqs_category', // Default taxonomy
+  ), $atts, 'faq_categories');
 
+  // Query arguments
+  $args = array(
+    'taxonomy'   => $atts['taxonomy'],
+    'title_li'   => '', // Remove title
+    'orderby'    => $atts['orderby'],
+    'order'      => $atts['order'],
+    'hide_empty' => $atts['hide_empty'],
+    'echo'       => false, // Don't output directly
+  );
 
-function recent_faqs_shortcode($atts) {
-    // Shortcode attributes
-    $atts = shortcode_atts(array(
-        'number' => 5, // Default number of FAQs to display
-    ), $atts, 'recent_faqs');
+  // Get categories list
+  $categories_list = wp_list_categories($args);
 
-    // Query arguments
-    $args = array(
-        'post_type' => 'faqs',
-        'posts_per_page' => $atts['number'],
-        'orderby' => 'date',
-        'order' => 'DESC',
-    );
-
-    // The Query
-    $faqs_query = new WP_Query($args);
-
-    // Check if there are any FAQs
-    if ($faqs_query->have_posts()) {
-        $output = '<ul>';
-        // Loop through FAQs
-        while ($faqs_query->have_posts()) {
-            $faqs_query->the_post();
-            $output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
-        }
-        $output .= '</ul>';
-        // Restore original Post Data
-        wp_reset_postdata();
-    } else {
-        $output = 'No recent FAQs found.';
-    }
-
-    return $output;
-}
-add_shortcode('recent_faqs', 'recent_faqs_shortcode');
-
-function faq_categories_shortcode($atts) {
-    // Shortcode attributes
-    $atts = shortcode_atts(array(
-        'orderby' => 'name', // Default ordering
-        'order' => 'ASC', // Default order
-        'hide_empty' => false, // Show even if categories have no posts
-        'taxonomy' => 'faqs_category', // Default taxonomy
-    ), $atts, 'faq_categories');
-
-    // Query arguments
-    $args = array(
-        'taxonomy'   => $atts['taxonomy'],
-        'title_li'   => '', // Remove title
-        'orderby'    => $atts['orderby'],
-        'order'      => $atts['order'],
-        'hide_empty' => $atts['hide_empty'],
-        'echo'       => false, // Don't output directly
-    );
-
-    // Get categories list
-    $categories_list = wp_list_categories($args);
-
-    // Check if any categories exist
-    if ($categories_list) {
-        return '<ul>' . $categories_list . '</ul>';
-    } else {
-        return 'No categories found.';
-    }
+  // Check if any categories exist
+  if ($categories_list) {
+    return '<ul>' . $categories_list . '</ul>';
+  } else {
+    return 'No categories found.';
+  }
 }
 add_shortcode('faq_categories', 'faq_categories_shortcode');
 
 
-/* CUSTOM POST TYPES */
-function custom_post_types() {
-    register_post_type( 'faqs',
+/* CUSTOM POST TYPE */
+function custom_post_types()
+{
+  register_post_type(
+    'faqs',
     // CPT Options
-        array(
-            'labels' => array(
-                'name' => __( 'Faqs' ),
-                'singular_name' => __( 'Faq' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'faqs'),
-            'show_in_rest' => true,
-            'menu_icon' => 'dashicons-clipboard',
-            'supports' => array( 'title', 'thumbnail', 'excerpt', 'editor' )
-			
-        )
-    );
-}
-add_action( 'init', 'custom_post_types' );
+    array(
+      'labels' => array(
+        'name' => __('FAQs'),
+        'singular_name' => __('FAQ'),
+        'add_new_item' => __('Add New FAQ'),
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'faqs'),
+      'show_in_rest' => true,
+      'menu_icon' => 'dashicons-clipboard',
+      'supports' => array('title', 'thumbnail', 'excerpt', 'editor')
 
-/* CATEGORY TAXONOMY */
-add_action( 'init', 'faq_category_taxonomy', 0 );
- 
-function faq_category_taxonomy() {
+    )
+  );
+}
+add_action('init', 'custom_post_types');
+
+/* FAQ CATEGORY TAXONOMY */
+add_action('init', 'faq_category_taxonomy', 0);
+
+function faq_category_taxonomy()
+{
   $labels = array(
-    'name' => _x( 'Faqs', 'taxonomy general name' ),
-    'singular_name' => _x( 'Faq', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Faqs' ),
-    'all_items' => __( 'All Faqs' ),
-    'parent_item' => __( 'Parent faq' ),
-    'parent_item_colon' => __( 'Parent faq:' ),
-    'edit_item' => __( 'Edit faq' ), 
-    'update_item' => __( 'Update Faq' ),
-    'add_new_item' => __( 'Add New Faq' ),
-    'new_item_name' => __( 'New Faq Name' ),
-    'menu_name' => __( 'Categories' ),
-  );    
-  register_taxonomy('faqs_category',array('faqs'), array(
+    'name' => _x('FAQs', 'taxonomy general name'),
+    'singular_name' => _x('FAQ', 'taxonomy singular name'),
+    'search_items' =>  __('Search FAQs'),
+    'all_items' => __('All FAQs'),
+    'parent_item' => __('Parent FAQ'),
+    'parent_item_colon' => __('Parent FAQ:'),
+    'edit_item' => __('Edit FAQ'),
+    'update_item' => __('Update FAQ'),
+    'add_new_item' => __('Add New FAQ'),
+    'new_item_name' => __('New FAQ Name'),
+    'menu_name' => __('Categories'),
+  );
+  register_taxonomy('faqs_category', array('faqs'), array(
     'hierarchical' => true,
     'labels' => $labels,
     'show_ui' => true,
     'show_admin_column' => true,
     'query_var' => true,
-    'rewrite' => array( 'slug' => 'faq' ),
+    'rewrite' => array('slug' => 'faq'),
   ));
 }
-
-function assign_faq_template_to_category_archive( $template ) {
-    error_log( 'Template function called.' ); // Check if the function is called
-    if ( is_tax('faqs_category') && !is_feed() ) {
-        error_log( 'Inside is_tax condition.' ); // Check if the condition is met
-        $new_template = locate_template( array( 'template-faqs.php' ) );
-        if ( '' != $new_template ) {
-            error_log( 'Template found.' ); // Check if the template file is found
-            return $new_template ;
-        }
-    }
-    error_log( 'Template not found.' ); // Check if the template file is not found
-    return $template;
-}
-add_filter( 'template_include', 'assign_faq_template_to_category_archive' );
-
---
+++ wp-phillipsandblow-theme/taxonomy-faqs_category.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * Template Name: FAQs Category
+ */
+
+get_header();
+get_template_part('blog-header');
+?>
+
+<div class="default-page has-sidebar" id="internal-page">
+  <div class="default-page-container has-sidebar inner-container">
+    <div class="default-main content">
+      <h1 class="main-heading"><?php single_term_title(); ?></h1>
+      <?php
+      echo do_shortcode('[faqs_category_filter]');
+      ?>
+      <div class="blog-posts test">
+
+        <?php
+        // Custom query to fetch FAQs for the current FAQ category
+        $args = array(
+          'post_type' => 'faqs',
+          'posts_per_page' => -1, // Display all FAQs
+          'tax_query' => array(
+            array(
+              'taxonomy' => 'faqs_category',
+              'field'    => 'slug',
+              'terms'    => get_queried_object()->slug,
+            ),
+          ),
+        );
+        $faq_query = new WP_Query($args);
+        if ($faq_query->have_posts()) :
+          while ($faq_query->have_posts()) : $faq_query->the_post();
+        ?>
+            <div class="blog-post">
+              <h2 class="blog-title"><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></h2>
+              <!-- <p class="blog-time"><?php the_time('F j, Y'); ?> Posted In <?php the_category(','); ?>  <br> by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> </p> -->
+              <?php the_excerpt(); ?>
+              <a href="<?php echo get_permalink() ?>" class="read-more">
+                read more
+              </a>
+            </div>
+        <?php
+          endwhile;
+          wp_reset_postdata(); // Reset the query
+        else :
+          echo 'Sorry, no FAQs found.';
+        endif;
+        ?>
+      </div>
+      <div class="navigation">
+        <?php wp_pagenavi(); ?>
+      </div>
+    </div>
+    <div class="default-sidebar faqs">
+      <div id="text-5" class="widget widget_text">
+        <h3 class="widget-title widgettitle">FAQ Categories</h3>
+        <div class="textwidget">
+          <ul>
+            <?php
+            // Retrieve list of FAQ categories dynamically
+            $faq_categories = get_terms(array(
+              'taxonomy' => 'faqs_category',
+              'hide_empty' => false,
+            ));
+
+            if (!empty($faq_categories)) {
+              foreach ($faq_categories as $faq_category) {
+                echo '<li class="cat-item cat-item-' . $faq_category->term_id . '"><a href="' . get_term_link($faq_category) . '">' . $faq_category->name . '</a></li>';
+              }
+            }
+            ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
